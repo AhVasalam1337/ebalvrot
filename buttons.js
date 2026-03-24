fe95ae34e5e96b6a74ccfcf85bdf615fffe8cb2d
AhVasalam1337 <64683917+AhVasalam1337@users.noreply.github.com>
2026-03-25 00:58:32 +0300

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -1,1 +1,11 @@
-
+// buttons.js
+export const mainKeyboard = {
+    reply_markup: {
+        keyboard: [
+            [{ text: "✨ Поболтать" }, { text: "📅 Планы" }],
+            [{ text: "💡 Идея" }, { text: "⚙️ Настройки" }]
+        ],
+        resize_keyboard: true,
+        one_time_keyboard: false
+    }
+};
